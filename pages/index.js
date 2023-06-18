4e5d08c7e53205fd9641c3fd4035d7b6b750ae16
Adauto Filho <92762447+adautodf@users.noreply.github.com>
2023-06-18 20:48:59 +0000

    acréscimo de emoji

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Larissa, meu amor! Estou fazendo isso para todos verem que eu a amo. </h1>
+    return <h1>Larissa, meu amor! Estou fazendo isso para todos verem que eu a amo ❤️ </h1>
 }
 
 export default Home;
