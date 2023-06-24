43baf283c866c96952909c09e3481f8d238dab4c
Adauto Filho <92762447+adautodf@users.noreply.github.com>
2023-06-24 03:55:35 +0000

    adição de scripts como `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 projeto em que estou me desafiando a aprimorar minhas habilidades em programação.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Larissa, meu amor! Estou fazendo isso para todos verem que eu a amo ❤️ </h1>
+function Home() {
+  return (
+    <h1>
+      Larissa, meu amor! Estou fazendo isso para todos verem que eu a amo ❤️{" "}
+    </h1>
+  );
 }
 
 export default Home;
